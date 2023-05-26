49781f3f74e2aac2c0fcf60f6c5d889696c6dbf1
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2023-05-25 22:59:41 -0700

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
+import About from './components/pages/About'
+import Portfolio from './components/pages/Portfolio'
+import Contact from './components/pages/Contact'
+import Resume from './components/pages/Resume'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+
+    <BrowserRouter>
+    <Navigation/>
+    <Routes>
+      <Route path="/" element={<About/>}/>
+      <Route path="/portfolio" element={<Portfolio/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/resume" element={<Resume/>}/>
+    </Routes>
+  </BrowserRouter>
+    </header>
+
+  <Footer/>
+    </>
   );
 }
 
